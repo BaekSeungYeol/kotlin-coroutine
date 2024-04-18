ed9144adbef0b56a3efd84e87c57cb93e88e38d4
baekseungyeol <beck33333@naver.com>
2024-04-19 01:38:59 +0900

    ch8

--- src/main/kotlin/me/whitewin/kotlincoroutine/ch8/CoroutineJob.kt
+++ src/main/kotlin/me/whitewin/kotlincoroutine/ch8/CoroutineJob.kt
@@ -2,34 +2,59 @@ package me.whitewin.kotlincoroutine.ch8
 
 import kotlinx.coroutines.*
 import me.whitewin.kotlincoroutine.ch3.delay
+//
+//suspend fun main() = coroutineScope {
+//    val job = Job()
+//    println(job)
+//    job.complete()
+//    println(job)
+//
+//    val activeJob = launch {
+//        delay(1000)
+//    }
+//
+//    println(activeJob)
+//    activeJob.join()
+//    println(activeJob)
+//
+//    val lazyJob = launch(start = CoroutineStart.LAZY) {
+//        delay(1000)
+//    }
+//    println(lazyJob)
+//    lazyJob.start()
+//    println(lazyJob)
+//    lazyJob.join()
+//    println(lazyJob)
+//
+//    val job2: Job = launch {
+//        delay(1000)
+//        println("Test")
+//    }
+//
+//    val deferred: Deferred<String> = async {
+//        delay(1000)
+//        "Test"
+//    }
+//    val job3: Job = deferred
+//}
 
-suspend fun main() = coroutineScope {
-    val job = Job()
-    println(job)
-    job.complete()
-    println(job)
+fun main(): Unit = runBlocking {
+//    val name = CoroutineName("Some name")
+//    val job = Job()
+//    launch(name + job) {
+//        val childName = coroutineContext[CoroutineName]
+//        println(childName == name)
+//        val childJob = coroutineContext[Job]
+//        println(childJob == job)
+//        println(childJob == job.children.first())
+//    }
 
-    val activeJob = launch {
+    val job: Job = launch {
         delay(1000)
     }
 
-    println(activeJob)
-    activeJob.join()
-    println(activeJob)
-
-    val lazyJob = launch(start = CoroutineStart.LAZY) {
-        delay(1000)
-    }
-    println(lazyJob)
-    lazyJob.start()
-    println(lazyJob)
-    lazyJob.join()
-    println(lazyJob)
-
-    val job2: Job = launch {
-        delay(1000)
-        println("Test")
-    }
-
-
+    val parentJob: Job = coroutineContext.job
+    println(job == parentJob)
+    val parentChildren: Sequence<Job> = parentJob.children
+    println(parentChildren.first() == job)
 }
--- src/main/kotlin/me/whitewin/kotlincoroutine/ch8/ParentChild.kt
+++ src/main/kotlin/me/whitewin/kotlincoroutine/ch8/ParentChild.kt
@@ -5,12 +5,12 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import me.whitewin.kotlincoroutine.ch3.delay
 
-fun main(): Unit = runBlocking(CoroutineName("main")) {
-    val name = coroutineContext[CoroutineName]?.name
-    println(name)
-    launch {
-        delay(1000)
-        val name = coroutineContext[CoroutineName]?.name
-        println(name)
-    }
-}
+//fun main(): Unit = runBlocking(CoroutineName("main")) {
+//    val name = coroutineContext[CoroutineName]?.name
+//    println(name)
+//    launch {
+//        delay(1000)
+//        val name = coroutineContext[CoroutineName]?.name
+//        println(name)
+//    }
+//}
