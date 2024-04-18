d13892544cbfc3e0fac5799914b29d484be46dd1
baekseungyeol <beck33333@naver.com>
2024-04-19 01:22:56 +0900

    ch8

--- src/main/kotlin/me/whitewin/kotlincoroutine/ch8/CoroutineJob.kt
+++ src/main/kotlin/me/whitewin/kotlincoroutine/ch8/CoroutineJob.kt
@@ -1,4 +1,29 @@
 package me.whitewin.kotlincoroutine.ch8
 
-class CoroutineJob {
+import kotlinx.coroutines.*
+import me.whitewin.kotlincoroutine.ch3.delay
+
+suspend fun main() = coroutineScope {
+    val job = Job()
+    println(job)
+    job.complete()
+    println(job)
+
+    val activeJob = launch {
+        delay(1000)
+    }
+
+    println(activeJob)
+    activeJob.join()
+    println(activeJob)
+
+    val lazyJob = launch(start = CoroutineStart.LAZY) {
+        delay(1000)
+    }
+    println(lazyJob)
+    lazyJob.start()
+    println(lazyJob)
+    lazyJob.join()
+    println(lazyJob)
+
 }
--- src/main/kotlin/me/whitewin/kotlincoroutine/ch8/ParentChild.kt
+++ src/main/kotlin/me/whitewin/kotlincoroutine/ch8/ParentChild.kt
@@ -1,4 +1,16 @@
 package me.whitewin.kotlincoroutine.ch8
 
-class ParentChild {
+import kotlinx.coroutines.CoroutineName
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+import me.whitewin.kotlincoroutine.ch3.delay
+
+fun main(): Unit = runBlocking(CoroutineName("main")) {
+    val name = coroutineContext[CoroutineName]?.name
+    println(name)
+    launch {
+        delay(1000)
+        val name = coroutineContext[CoroutineName]?.name
+        println(name)
+    }
 }
