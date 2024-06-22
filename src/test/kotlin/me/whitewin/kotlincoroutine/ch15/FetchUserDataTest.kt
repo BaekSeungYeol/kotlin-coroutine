855201e8a0e8f2fb7be0904b93b898c2c699a822
baekseungyeol <beck33333@naver.com>
2024-06-22 22:08:37 +0900

    ch15

--- src/test/kotlin/me/whitewin/kotlincoroutine/ch15/FetchUserDataTest.kt
+++ src/test/kotlin/me/whitewin/kotlincoroutine/ch15/FetchUserDataTest.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.test.currentTime
 import kotlinx.coroutines.test.runTest
 import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
+import kotlin.coroutines.CoroutineContext
 
 class FetchUserDataTest {
 
@@ -90,6 +91,27 @@ class FetchUserDataTest {
     ): List<R> = coroutineScope {
         this@mapAsync.map { async { transformation(it) }}
             .awaitAll()
+    }
+
+    @Test
+    fun `should support context propagation`() = runTest {
+        var ctx: CoroutineContext? = null
+        val name1 = CoroutineName("Name 1")
+        withContext(name1) {
+            listOf("A").mapAsync {
+                ctx = currentCoroutineContext()
+                it
+            }
+            assertEquals(name1, ctx?.get(CoroutineName))
+        }
+        val name2 = CoroutineName("Name 2")
+        withContext(name2) {
+            listOf(1,2,3,).mapAsync {
+                ctx = currentCoroutineContext()
+                it
+            }
+            assertEquals(name2, ctx?.get(CoroutineName))
+        }
     }
 
 }
